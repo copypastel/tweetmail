97472e1930338a4a81271532fd0d672e648d2c76
daicoden <daicoden@copypastel.com>
2009-04-13 03:21:50 -0600

    Example using a test tweet class which is compatable with tweet_ext.  Running tweet_mail.rb will send ecin an email.

--- lib/tweet_mail.rb
+++ lib/tweet_mail.rb
@@ -1,7 +1,7 @@
 #tweeter is the tweet mailer.
 require 'rubygems'
 require 'actionmailer'
-require "smtp_tls"
+require File.join(File.dirname(__FILE__), "smtp_tls")
 
 # Actionmailer config
 ActionMailer::Base.raise_delivery_errors = true
@@ -22,18 +22,28 @@ class Notifier < ActionMailer::Base
      from       "tweetmail@copypastel.com"
      subject    "You have new tweets"
      body       formatted_tweets
+     content_type "text/html"
   end
 end
 
 module TweetMail
-  def self.send(email,tweets)
-    Notifier.deliver_tweet_update(email,format_tweets(tweets))
+  def self.send(email,tweets = [])
+    Notifier.deliver_tweet_update(email,format_tweets(tweets)) unless tweets == []
   end
   
   def self.format_tweets(tweets)
-    # Fill this out!
-    tweets
+    html = '<center><b>You have tweets!</b></center><br/><br/>'
+    tweets.each do |tweet|
+      html += "<a href='http://twitter.com/#{tweet.from}'>#{tweet.from}</a><br/>"
+      html += tweet.msg
+      html += "<br/><br/>"
+    end
+    html
   end
 end
 
-TweetMail.send("ecin@copypastel.com","Hello")
+class TestTweet < Struct.new(:from,:msg)
+end
+
+TweetMail.send("ecin@copypastel.com",[TestTweet.new("Bob","Hello Man"),TestTweet.new("ecin","I like gay boys.")])
+puts "done"
