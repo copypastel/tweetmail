99ef5f4abc199d10458f77a8f03fe15fe6564193
ecin <ecin@copypastel.com>
2009-04-13 06:43:26 -0400

    Config files are now written out to a config folder.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,14 +4,14 @@ module TweetMailHelpers
   
   def write_config(username, email, replies)
     body = OpenStruct.new :username => username, :email => email, :since => replies.first.id
-    filename = username + '.yaml'
+    filename = 'config/' + username + '.yaml'
     config = File.open( filename, 'w' )
     config << YAML::dump(body)
     config.close
   end
   
   def read_config( username )
-    filename = username + '.yaml'
+    filename = 'config/' + username + '.yaml'
     file = File.open( filename, 'r' )
     config = YAML::load(file.read)
     file.close
