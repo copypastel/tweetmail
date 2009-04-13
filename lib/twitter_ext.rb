8d609f24f4db83d6592d23fd05286b5c4b15b516
ecin <ecin@copypastel.com>
2009-04-13 06:13:41 -0400

    Reply now stores the @id of tweets.

--- lib/twitter_ext.rb
+++ lib/twitter_ext.rb
@@ -1,14 +1,16 @@
 module TwitterExt
   
-  class Reply < Struct.new(:from, :to, :message, :when)
+  class Reply < Struct.new(:id, :from, :to, :message, :when)
     alias :msg :message
     alias :when? :when
   end
   
-  def Replies(username = nil)
+  def Replies(username = nil, since = nil)
     return [] if username.nil?
     replies = []
-    Twitter::Search.new.to(username).each {|t| replies << Reply.new(t.from_user, t.to_user, t.text, Time.parse(t.created_at))}
+    Twitter::Search.new.to(username).since(since).each do |t| 
+      replies << Reply.new( t.id, t.from_user, t.to_user, t.text, Time.parse(t.created_at) )
+    end
     replies
   end
   
