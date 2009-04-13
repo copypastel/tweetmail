3a022e0da3d1b4a9f5b6918a1db36175121cc835
daicoden <daicoden@copypastel.com>
2009-04-13 02:56:06 -0600

    An example of sending tweets is done, run ruby lib/tweet_mail.rb and ecin will get an email saying hello.

--- lib/tweet_mail.rb
+++ lib/tweet_mail.rb
@@ -1,21 +1,39 @@
 #tweeter is the tweet mailer.
-require 'simplemail'
+require 'rubygems'
+require 'actionmailer'
+require "smtp_tls"
+
+# Actionmailer config
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.gmail.com",
+  :port  => 587, 
+  :domain => 'copypastel.com',
+  :user_name  => "tweetmail@copypastel.com",
+  :password  => "tweetmailer",
+  :authentication  => :plain
+}
+
+
+class Notifier < ActionMailer::Base
+  def tweet_update(email,formatted_tweets)
+     recipients email
+     from       "tweetmail@copypastel.com"
+     subject    "You have new tweets"
+     body       formatted_tweets
+  end
+end
+
 module TweetMail
-  def send(dest,tweets)
-    ActionMailer::Base.raise_delivery_errors = true
-    ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.server_settings = {
-      :address  => "smtp.gmail.com",
-      :port  => 587, 
-      :domain => 'copypastel.com',
-      :user_name  => "tweetmail@yourdomain.com",
-      :password  => "tweetmailer",
-      :authentication  => :plain
-    }
+  def self.send(email,tweets)
+    Notifier.deliver_tweet_update(email,format_tweets(tweets))
   end
   
-  def format_tweets(tweets)
+  def self.format_tweets(tweets)
     # Fill this out!
     tweets
   end
 end
+
+TweetMail.send("ecin@copypastel.com","Hello")
