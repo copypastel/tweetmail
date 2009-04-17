148cf0c93b11d632096cdf741a14889db2c250bd
ecin <ecin@copypastel.com>
2009-04-16 20:02:50 -0400

    Modified switches in lib/tweetmail/command_line.rb.

--- lib/tweetmail/command_line.rb
+++ lib/tweetmail/command_line.rb
@@ -11,13 +11,13 @@ module Tweetmail
     }
 
     parser = OptionParser.new do |opts|
-      opts.banner = "Usage: tweetmail [username] [email]"
+      opts.banner = "Usage: tweetmail [-wh] [-i twitter_id] [-c config_file] [-s server_config_file] [username] [email]"
 
       opts.separator ""
       opts.separator "Options:"
 
-      opts.on("-d", "--date ID", "Email @reply tweets since a specified tweet id, e.g. --since 1378993302") do |d|
-        $OPTIONS[:since] = s
+      opts.on("-i", "--id ID", "Email @reply tweets since a specified tweet id, e.g. --id 1378993302") do |i|
+        $OPTIONS[:since] = i
         @validator[:since] = true
       end
 
@@ -26,17 +26,17 @@ module Tweetmail
         @validator[:config] = true
       end
 
-      opts.on("-s", "--server FILE", "Configuration file for server, e.g. --server config/server-settings.yaml") do |s|
+      opts.on("-s", "--server FILE", "SMTP configuration file for mail server, e.g. --server config/server-settings.yaml") do |s|
         $OPTIONS[:server] = s
         @validator[:server] = true
       end
 
-      opts.on("-w", "--write", "Write out configuration file after emailing the tweets.") do |n|
+      opts.on("-w", "--write", "Write out configuration file after emailing the tweets") do |n|
         $OPTIONS[:write_config] = n
         @validator[:write_config] = true
       end
 
-      opts.on_tail("-h", "--help", "Show this message.") do
+      opts.on_tail("-h", "--help", "Show this message") do
         puts opts
         exit
       end
